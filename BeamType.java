c0beedd5303286650c2478c062f617eea36ba5b9
Adei Josol <adjl@users.noreply.github.com>
2014-05-24 00:31:40 +0100

    Add BeamType enum

---
+++ BeamType.java
@@ -0,0 +1,40 @@
+enum BeamType {
+
+  FAR(1f, 0.1f, 3f, 0.5f, 5f),
+  MIDDLE(2f, 0.2f, 6f, 0.7f, 7f),
+  NEAR(3f, 0.3f, 9f, 0.9f, 9f);
+
+  final float velocity;
+  final float acceleration;
+  final float terminalVelocity;
+  final float opacity;
+  final float size;
+
+  BeamType(float velocity, float acceleration, float terminalVelocity, float opacity, float size) {
+    this.velocity = velocity;
+    this.acceleration = acceleration;
+    this.terminalVelocity = terminalVelocity;
+    this.opacity = opacity * 255;
+    this.size = size;
+  }
+
+  float velocity() {
+    return velocity;
+  }
+
+  float acceleration() {
+    return acceleration;
+  }
+
+  float terminalVelocity() {
+    return terminalVelocity;
+  }
+
+  float opacity() {
+    return opacity;
+  }
+
+  float size() {
+    return size;
+  }
+}
