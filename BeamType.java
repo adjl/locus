3edebba4ac6d21d694e3ecd0ca1ad83c758f3cb8
Adei Josol <adjl@users.noreply.github.com>
2014-05-26 01:46:44 +0100

    Change BeamTypes to SLOW, NORMAL and FAST

--- BeamType.java
+++ BeamType.java
@@ -1,8 +1,8 @@
 enum BeamType {
 
-  FAR(1f, 0.1f, 3f, 0.5f, 5f),
-  MIDDLE(2f, 0.2f, 6f, 0.7f, 7f),
-  NEAR(3f, 0.3f, 9f, 0.9f, 9f);
+  SLOW(1f, 0.1f, 3f, 0.5f, 5f),
+  NORMAL(2f, 0.2f, 6f, 0.7f, 7f),
+  FAST(3f, 0.3f, 9f, 0.9f, 9f);
 
   final float velocity;
   final float acceleration;
