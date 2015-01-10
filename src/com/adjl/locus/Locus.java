5bad208e21a4f33adde1e3a3d68966a68e22282c
Adei Josol <adjl@users.noreply.github.com>
2015-01-10 20:07:22 +0000

    Import as a Proclipsing project

---
+++ src/com/adjl/locus/Locus.java
@@ -0,0 +1,17 @@
+package com.adjl.locus;
+
+import processing.core.PApplet;
+
+
+public class Locus extends PApplet {
+
+	public void setup() {
+	}
+
+	public void draw() {
+	}
+	
+	public static void main(String _args[]) {
+		PApplet.main(new String[] { com.adjl.locus.Locus.class.getName() });
+	}
+}
